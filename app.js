25e2e1f49eecaefcae6ceb3e00c17426ad8e365f
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-25 10:11:43 +0000

    Combine get, post, delete into one route

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded ({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // connect to mongoose
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 // Create schema
 const articlesSchema = {
     title: String,
@@ -19,39 +19,34 @@ const articlesSchema = {
 // Create the model
 const Article = mongoose.model("Article", articlesSchema);
 
-// method to get all articles
-app.get("/articles", function(req, res) {
-    Article.find().then(function(docs) {
+// combine same routes
+app.route("/articles").get(function (req, res) {
+    // method to get all articles
+    Article.find().then(function (docs) {
         if (docs) {
             res.send(docs);
         }
     });
-});
-
-
-// method to post article
-app.post("/articles", function(req, res) {
+}).post(function (req, res) {
+    // method to post an article
     //console.log(req.body.title);
     //console.log(req.body.content);
-
     const newArticle = new Article({
         title: req.body.title,
         content: req.body.content
     });
 
-    newArticle.save().then(function(doc) {
+    newArticle.save().then(function (doc) {
         res.send("Successfully added a new article.");
     });
-});
-
-// method to delete all articles
-app.delete("/articles", function(req, res) {
-    Article.deleteMany().then(function(docs){
+}).delete(function (req, res) {
+    // method to delete all articles
+    Article.deleteMany().then(function (docs) {
         res.send("Successfully deleted all articles.");
     });
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
