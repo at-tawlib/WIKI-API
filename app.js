252214e9b243204b6923f33d636e4eb0175a06ea
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-04-25 09:48:45 +0000

    Add get, post and delete methods

--- app.js
+++ app.js
@@ -7,19 +7,49 @@ const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded ({extended: true}));
+app.use(express.static("public"));
 
 // connect to mongoose
-mongoose.connect("mongodb://localhost:2701/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 // Create schema
 const articlesSchema = {
     title: String,
     content: String
-}
+};
 // Create the model
 const Article = mongoose.model("Article", articlesSchema);
 
+// method to get all articles
+app.get("/articles", function(req, res) {
+    Article.find().then(function(docs) {
+        if (docs) {
+            res.send(docs);
+        }
+    });
+});
 
 
+// method to post article
+app.post("/articles", function(req, res) {
+    //console.log(req.body.title);
+    //console.log(req.body.content);
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save().then(function(doc) {
+        res.send("Successfully added a new article.");
+    });
+});
+
+// method to delete all articles
+app.delete("/articles", function(req, res) {
+    Article.deleteMany().then(function(docs){
+        res.send("Successfully deleted all articles.");
+    });
+});
 
 
 app.listen(3000, function(){
